d5b1f75b7d33fb5e69dbc1d3d353aaf8eebb4c49
Raphael Narasaki <narasakir@gmail.com>
2019-02-13 15:34:26 -0200

    markup app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-    render(){
-        return <h1>React App</h1>
-    }
+  render() {
+    return (
+      <div className='app'>
+        <div className='search'>
+        <input type='search' placeholder="Digite o nome do usuario no Github"/>
+        </div>
+        <div className='user-info'>
+          <img src='' />
+          <h1 className='user-name'>
+            <a href='#'>Raphael Narasaki</a>
+          </h1>
+          <ul className='repos-info'>
+            <li>- Repositorios: 122</li>
+            <li>- Seguidores: 10</li>
+            <li>- Seguindo: 10</li>
+          </ul>
+
+          <div className='actions'>
+            <button>Ver Repositorios</button>
+            <button>Ver Favoritos</button>
+          </div>
+
+          <div className='repos'>
+            <h2>Repositorios:</h2>
+            <ul>
+              <li><a href='#'>Nome do Repositorio</a></li>
+            </ul>
+          </div>
+
+          <div className='starred'>
+            <h2>Favoritos</h2>
+            <ul >
+              <li><a href='#'>Nome do Repositorio</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App

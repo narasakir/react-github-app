c712aa8225236e99e6b34ed400e5884182c6e199
Raphael Narasaki <narasakir@gmail.com>
2019-02-14 15:34:50 -0200

    add production build

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack.config')
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,30 +5,31 @@ const webpack = require('webpack')
 const validate = require('webpack-validator')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const DashBoardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = validate({
   devtool: 'source-map',
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, 'src', 'index')
-  ],
+  entry: path.join(__dirname, 'src', 'index'),
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name]-[hash].js',
-    publicPath: ''
+    filename: '[name]-[hash].js'
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new DashBoardPlugin(),
+    new ExtractTextPlugin('[name]-[hash].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new HtmlPlugin({
       title: 'Github App',
       template: path.join(__dirname, 'src', 'html', 'template.html')
     }),
-    new ExtractTextPlugin('[name]-[hash].css')
   ],
 
   module: {
@@ -50,7 +51,7 @@ module.exports = validate({
         test: /\.css$/,
         exclude: /node_modules/,
         include: /src/,
-        loaders: ['style', 'css']
+        loader: ExtractTextPlugin.extract('style', 'css')
       },
     ]
   }
